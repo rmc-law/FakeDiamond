847d4c24f455a83c529ff4b40c2e3c75c108b734
Ryam M.C. Law <ryanlaw7@gmail.com>
2024-02-13 22:08:31 +0000

    [fix] disentangle confounded triggers

--- scripts/preprocessing/02_preprocess_data.py
+++ scripts/preprocessing/02_preprocess_data.py
@@ -12,15 +12,12 @@ import os.path as op
 import pickle
 import json
 
-from mne import pick_types, find_events, pick_events, Epochs
+from mne import pick_types, find_events, Epochs
 from mne.preprocessing import ICA, create_eog_epochs, read_ica
 from autoreject import get_rejection_threshold
 
-from eelbrain.gui import select_epochs
-from eelbrain.load import unpickle
-
 import config
-from helper import check_preprocessed_files, read_raws, shift_event_photodiode, find_bad_chs
+import helper
 
 subjects = config.subject_ids
 runs = [1,2,3,4,5]
@@ -30,7 +27,7 @@ h_freq = config.h_freq
 
 preprocessed_data_path = '/imaging/hauk/rl05/fake_diamond/data/preprocessed'
 
-check_preprocessed_files(preprocessed_data_path, subjects)
+helper.check_preprocessed_files(preprocessed_data_path, subjects)
 
 subject = input('sub to preprocess: ')
 subject = f'sub-{subject}'
@@ -38,10 +35,9 @@ ica_path = op.join(preprocessed_data_path, subject, 'ica')
 ica_fname = op.join(ica_path, f'{subject}_ica.fif')
 epoch_path = op.join(preprocessed_data_path, subject, 'epoch')
 epoch_fname = op.join(epoch_path, f'{subject}_epo.fif')
-epoch_rej_fname = op.join(epoch_path, f'{subject}_epo_rej.pickled')
 
 # concatenate raws from different runs/blocks
-raw = read_raws(preprocessed_data_path, subject, runs=runs)
+raw = helper.read_raws(preprocessed_data_path, subject, runs=runs)
 raw.load_data(verbose=False)
 
 
@@ -65,17 +61,14 @@ raw.info['bads'] += config.bad_chs[subject[-2:]]['meg']
 autoreject_bad_chs_fname = op.join(epoch_path, 'autoreject_bad_chs.txt')
 if op.exists(autoreject_bad_chs_fname):
     with open(autoreject_bad_chs_fname, 'r') as file:
-        content = file.readlines()
-    autoreject_bad_chs = [line.strip() for line in content]
-    print('autoreject detected bad channels: ', autoreject_bad_chs)
+        autoreject_bad_chs = [line.strip() for line in file.readlines()]
 else:
-    autoreject_bad_chs = find_bad_chs(raw)
-    if autoreject_bad_chs:
-        with open(autoreject_bad_chs_fname, 'w') as file:
-            for bad_ch in autoreject_bad_chs[0]:
-                file.write(f'{bad_ch}\n')
-        autoreject_bad_chs = autoreject_bad_chs[0]
+    autoreject_bad_chs = helper.find_bad_chs(raw)
+    with open(autoreject_bad_chs_fname, 'w') as file:
+        file.writelines(f'{bad_ch}\n' for bad_ch in autoreject_bad_chs)
 raw.info['bads'] += autoreject_bad_chs
+raw.info['bads'] = list(set(raw.info['bads'])) # if there are overlaps
+print('autoreject detected bad channels:', autoreject_bad_chs)
 print('Bad channels: ', raw.info['bads'])
 
 # set eeg reference
@@ -117,7 +110,7 @@ else:
     
     # visual inspection after ICA component rejection
     for ch_type in ['eeg','meg']:
-        ica_overlay = ica.plot_overlay(raw, picks=ch_type, start=4., stop=7.)
+        ica_overlay = ica.plot_overlay(raw, picks=ch_type, start=8., stop=10.)
         ica_overlay_fname = op.join(ica_path, f'ica_overlay_{ch_type}.png')
         ica_overlay.savefig(ica_overlay_fname)
     
@@ -132,51 +125,33 @@ ica.apply(raw)
 # Epoch data and reject bad epochs
 # =============================================================================
 
-print('Bad channels: ', raw.info['bads'])
-
 # help find bad channels using butterfly mode
-raw.plot(butterfly=True)
+# raw.plot(butterfly=True)
 
-# if raw.info['bads']:
-#     print('Interpolating bads.')
-#     raw.interpolate_bads(reset_bads=True, mode='accurate')
+if raw.info['bads']:
+    print('Interpolating bads.')
+    raw.interpolate_bads(reset_bads=False, mode='accurate')
+print('Bad channels: ', raw.info['bads'])
 
 # epoch parameters
 tmin = -0.5 # include pre-fixation period for noise covariance
-tmax_epo = 1.4 # include both words
+tmax = 1.4  # include both words
 baseline = (-0.5,-0.3)
-picks = pick_types(raw.info, 
-                   meg=True, 
-                   eeg=True, 
-                   exclude=() # not excluding bads to ensure bads info stay in epochs objects
-                   ) 
-events_all = find_events(raw, stim_channel='STI101', min_duration=0.005)
+picks = pick_types(raw.info, meg=True, eeg=True, exclude=())  # not excluding bads to ensure bads info stay in epochs objects
+
+# find semantic and photodiode events
+events_semantic = helper.find_semantic_events(raw, subject=subject, semantic_triggers=list(config.event_id_semantic_word1.values()))
+assert(len(events_semantic) == 900) # sanity
+events_photodiode = find_events(raw, stim_channel='STI010', min_duration=0.002, verbose=False)
 
 # shift events by photodiode delay
-delay = shift_event_photodiode(events_all, 
-                               target_triggers=list(config.event_id_semantic.values()),
-                               photodiode_trigger=512
-                               )
-event_id = config.event_id_semantic
-events_semantic = pick_events(events_all, include=list(event_id.values()))
-
-# Check to see if photodiode and target events match:
-if len(events_semantic) == len(delay):
-    print('Matching number of events! Adding the shift!')
-    events_semantic[:,0] = events_semantic[:,0] + delay
-else:
-    print('Mismatching number of events :( using mean shift')
-    events_semantic[:,0] = events_semantic[:,0] + round(delay.mean())
-
-
-event_id = config.event_id_semantic_word1
-events = pick_events(events_semantic, include=list(event_id.values()))
+events_semantic_corrected = helper.correct_display_delay(events_semantic, events_photodiode)
 
 # segment continuous data into epoch
 print('Segmenting into epochs.')
-epochs = Epochs(raw, events, event_id=event_id, 
+epochs = Epochs(raw, events_semantic_corrected, event_id=config.event_id_semantic_word1, 
                 tmin=tmin, 
-                tmax=tmax_epo, 
+                tmax=tmax, 
                 baseline=baseline, 
                 preload=True, 
                 picks=picks, 
@@ -184,28 +159,28 @@ epochs = Epochs(raw, events, event_id=event_id,
                 verbose=False)
 print('Number of epochs found: ', len(epochs))
 
-
 print('Resampling epochs to 250Hz for speed.')
 epochs = epochs.resample(sfreq=epochs.info['sfreq']/4)
-
-
 
 # automatic rejection of bad epochs
 autoreject_threshold_fname = op.join(epoch_path, 'autoreject_threshold.json')
 if op.exists(autoreject_threshold_fname):
-    with open(autoreject_threshold_fname, 'r') as file:
-        reject = json.load(autoreject_threshold_fname)    
-    print('empirically determined thresholds: ', reject)
+    with open(autoreject_threshold_fname, 'rb') as file:
+        autoreject_thresholds = json.load(file)    
+    print('empirically determined thresholds: ', autoreject_thresholds)
 else:
-    reject = get_rejection_threshold(epochs, random_state=42)
-    print('The rejection dictionary is %s' % reject)
+    autoreject_thresholds = get_rejection_threshold(epochs, random_state=42)
+    print('The rejection dictionary is %s' % autoreject_thresholds)
     with open(autoreject_threshold_fname, 'w') as file:
-        json.dump(reject, file)
+        json.dump(autoreject_thresholds, file)
         
-epochs.drop_bad(reject=reject)
-evoked = epochs.average()
-figs_evoked = evoked.plot_joint(show=False, times=[0.17,0.77,1.0])
-# figs_evoked.savefig(op.join(epoch_path, f'{subject}_autoreject_grand-evoked.png'))
+epochs.drop_bad(reject=autoreject_thresholds)
+
+# if automatic rejection did not drop any epochs, use hardcoded threshold
+if len(epochs) == 900: 
+    composite_rejection_thresholds = helper.compare_reject_thresholds(autoreject_thresholds, config.hardcoded_thresholds)
+    epochs.drop_bad(reject=composite_rejection_thresholds)
+    
 
 drop_log_fname = op.join(epoch_path, 'epochs_drop_log.pkl')
 with open(drop_log_fname, 'wb') as file:
@@ -214,15 +189,16 @@ with open(drop_log_fname, 'wb') as file:
 figs_plot_drop_log = epochs.plot_drop_log(show=False)
 figs_plot_drop_log.savefig(op.join(epoch_path, 'epochs_drop_log_stats.png'))
 
-
-# # epoch rejection using eelbrain
-# select_epochs(epochs, mark=['MEG0112'])
-
-# rejfile = unpickle(epoch_rej_fname)
-# rejs = rejfile['accept'].x
-# epochs = epochs[rejs]
-
 # plot joint to see if it's quite clean
-figs_evoked = epochs.average().plot_joint()
+figs_evoked = epochs.average().plot_joint(times=[0, 0.17, 0.6, 0.77, 1.0],
+                                          exclude='bads',
+                                          show=False)
+for fig_evoked, ch_type in zip(figs_evoked, ['eeg', 'mag', 'grad']):
+    fig_evoked.savefig(op.join(epoch_path, f'fig_sensor_{ch_type}_grand_ave.png'))
+
+
+# visually inspect epochs
+epochs.plot(butterfly=True)
+
 
 epochs.save(epoch_fname, overwrite=True)
--- scripts/preprocessing/helper.py
+++ scripts/preprocessing/helper.py
@@ -10,18 +10,19 @@ import os
 import os.path as op
 import shutil
 import numpy as np
-# import pandas as pd
+import pandas as pd
 from glob import glob
 
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+from scipy.stats import binom
 
-from mne import find_events, pick_events, concatenate_raws, Epochs, pick_types
-from mne.io import read_raw_fif
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import mne
 # from mne.viz import tight_layout
+
 from autoreject import Ransac  # noqa
 # from autoreject.utils import interpolate_bads  # noqa
-
 
 import config
 
@@ -30,6 +31,10 @@ cbu_repo_meg = '/megdata/cbu'
 cbu_repo_mri = '/mridata/cbu'
 project_repos = '/imaging/hauk/rl05'
 
+
+# =============================================================================
+# 
+# =============================================================================
 
 def check_preprocessed_files(preprocessed_data_path, subjects):
     for subject in subjects:
@@ -55,42 +60,6 @@ def check_preprocessed_files(preprocessed_data_path, subjects):
             print(f'epoch: {subject} epoch fif not computed.')
         print()
 
-
-def read_raws(preprocessed_data_path, subject, runs):
-    print('Reading raws from all runs.')
-    raws = []
-    for run in runs:
-        # print('run', run)
-        raw_sss_fname = op.join(preprocessed_data_path, subject, 'maxfiltered', f'run-{run}', f'run{run}_sss_raw.fif')
-        raw = read_raw_fif(raw_sss_fname, verbose=False)
-        picks = pick_types(raw.info, meg=True, eeg=True, stim=True, eog=True, chpi=False)
-        raw = raw.pick(picks)
-        raws.append(raw)
-    # check if all raws have same number of channels
-    if not all([raw.info['nchan']==raws[0].info['nchan'] for raw in raws]):
-        print('Raw fifs have different numbers of channels.')
-        raws = [raw.drop_channels('STI010') if 'STI010' in raw.info['ch_names'] else raw for raw in raws]
-    concatenated_raw = concatenate_raws(raws)
-    print('Done.')
-    return concatenated_raw
-
-def find_bad_chs(raw):
-    event_id = config.event_id_semantic_word1
-    events = find_events(raw, min_duration=0.005)
-    events = pick_events(events, include=list(event_id.values()))
-    bad_chs = []
-    if raw.info['projs']:
-        raw.del_proj()
-    epochs = Epochs(raw, events, picks='data', tmin=-0.5, tmax=1.4, reject=None,
-                    baseline=(None, -0.3), detrend=0, preload=True, verbose=False)
-    for ch_type in ['eeg','mag','grad']: # ransac can only do on ch_type at a time
-        ransac = Ransac(verbose=True, picks=ch_type, n_jobs=1, random_state=42)
-        ransac.fit(epochs)
-        if ransac.bad_chs_:
-            bad_chs.append(ransac.bad_chs_)
-    return bad_chs
-
-
 def copy_log_files(project, subject):
     destination = op.join(project_repos, project, 'data', 'logs', subject)
     if not op.isdir(destination):  
@@ -103,116 +72,200 @@ def copy_log_files(project, subject):
             shutil.copy2(source, destination)
             
 
-# def plot_noisy_channel_detection(auto_scores, ch_type):
-#     # Your existing code here
-#     ch_subset = auto_scores["ch_types"] == ch_type
-#     ch_names = auto_scores["ch_names"][ch_subset]
-#     scores = auto_scores["scores_noisy"][ch_subset]
-#     limits = auto_scores["limits_noisy"][ch_subset]
-#     bins = auto_scores["bins"]
-#     bin_labels = [f"{start:3.3f} – {stop:3.3f}" for start, stop in bins]
-#     data_to_plot = pd.DataFrame(
-#         data=scores,
-#         columns=pd.Index(bin_labels, name="Time (s)"),
-#         index=pd.Index(ch_names, name="Channel"),
-#     )
 
-#     fig, ax = plt.subplots(1, 2, figsize=(12, 8))
-#     fig.suptitle(
-#         f"Automated noisy channel detection: {ch_type}", fontsize=16, fontweight="bold"
-#     )
-#     sns.heatmap(data=data_to_plot, cmap="Reds", cbar_kws=dict(label="Score"), ax=ax[0])
-#     [
-#         ax[0].axvline(x, ls="dashed", lw=0.25, dashes=(25, 15), color="gray")
-#         for x in range(1, len(bins))
-#     ]
-#     ax[0].set_title("All Scores", fontweight="bold")
+# =============================================================================
+# Continuous data preprocessing
+# =============================================================================
 
-#     sns.heatmap(
-#         data=data_to_plot,
-#         vmin=np.nanmin(limits),
-#         cmap="Reds",
-#         cbar_kws=dict(label="Score"),
-#         ax=ax[1],
-#     )
-#     [
-#         ax[1].axvline(x, ls="dashed", lw=0.25, dashes=(25, 15), color="gray")
-#         for x in range(1, len(bins))
-#     ]
-#     ax[1].set_title("Scores > Limit", fontweight="bold")
+def read_raws(preprocessed_data_path, subject, runs):
+    print('Reading raws from all runs.')
+    raws = []
+    for run in runs:
+        # print('run', run)
+        raw_sss_fname = op.join(preprocessed_data_path, subject, 'maxfiltered', f'run-{run}', f'run{run}_sss_raw.fif')
+        raw = mne.io.read_raw_fif(raw_sss_fname, verbose=False)
+        picks = mne.pick_types(raw.info, meg=True, eeg=True, stim=True, eog=True, chpi=False)
+        raw = raw.pick(picks)
+        raws.append(raw)
+    # check if all raws have same number of channels
+    if not all([raw.info['nchan']==raws[0].info['nchan'] for raw in raws]):
+        print('Raw fifs have different numbers of channels.')
+        raws = [raw.drop_channels('STI010') if 'STI010' in raw.info['ch_names'] else raw for raw in raws]
+    concatenated_raw = mne.concatenate_raws(raws)
+    print('Done.')
+    return concatenated_raw
 
-#     fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-#     return fig
+def find_bad_chs(raw):
+    event_id = config.event_id_semantic_word1
+    events = mne.find_events(raw, min_duration=0.005)
+    events = mne.pick_events(events, include=list(event_id.values()))
+    bad_chs = []
+    if raw.info['projs']:
+        raw.del_proj()
+    epochs = mne.Epochs(raw, events, picks='data', tmin=-0.5, tmax=1.4, reject=None,
+                    baseline=(None, -0.3), detrend=0, preload=True, verbose=False)
+    for ch_type in ['eeg','mag','grad']: # ransac can only do on ch_type at a time
+        ransac = Ransac(verbose=True, picks=ch_type, n_jobs=1, random_state=42)
+        ransac.fit(epochs)
+        if ransac.bad_chs_:
+            bad_chs.append(ransac.bad_chs_)
+    return bad_chs
 
+def plot_noisy_channel_detection(auto_scores, ch_type):
+    # Your existing code here
+    ch_subset = auto_scores["ch_types"] == ch_type
+    ch_names = auto_scores["ch_names"][ch_subset]
+    scores = auto_scores["scores_noisy"][ch_subset]
+    limits = auto_scores["limits_noisy"][ch_subset]
+    bins = auto_scores["bins"]
+    bin_labels = [f"{start:3.3f} – {stop:3.3f}" for start, stop in bins]
+    data_to_plot = pd.DataFrame(
+        data=scores,
+        columns=pd.Index(bin_labels, name="Time (s)"),
+        index=pd.Index(ch_names, name="Channel"),
+    )
 
-# def filter_raw(raw):
-#     # this section visualises the effects of filtering on sensor signals
-#     # make a copy of the original channels
-#     print ('Making a copy of raw data...')
-#     x0 = raw.copy().pick(['MEG1113']).crop(tmin = 60., tmax = 120.) # unfiltered
+    fig, ax = plt.subplots(1, 2, figsize=(12, 8))
+    fig.suptitle(
+        f"Automated noisy channel detection: {ch_type}", fontsize=16, fontweight="bold"
+    )
+    sns.heatmap(data=data_to_plot, cmap="Reds", cbar_kws=dict(label="Score"), ax=ax[0])
+    [
+        ax[0].axvline(x, ls="dashed", lw=0.25, dashes=(25, 15), color="gray")
+        for x in range(1, len(bins))
+    ]
+    ax[0].set_title("All Scores", fontweight="bold")
 
-#     # apply band-pass filter to raw
-#     l_freq = 0 # high-pass filter in Hz
-#     h_freq = 40 # low-pass filter in Hz
-#     raw_filtered = raw.filter(l_freq, h_freq, method='fir', fir_window='hamming', fir_design='firwin')
-#     # the method argument is default to fir in MNE 0.17 but iir in previous versions
+    sns.heatmap(
+        data=data_to_plot,
+        vmin=np.nanmin(limits),
+        cmap="Reds",
+        cbar_kws=dict(label="Score"),
+        ax=ax[1],
+    )
+    [
+        ax[1].axvline(x, ls="dashed", lw=0.25, dashes=(25, 15), color="gray")
+        for x in range(1, len(bins))
+    ]
+    ax[1].set_title("Scores > Limit", fontweight="bold")
 
-#     # then make a copy of filtered channels
-#     print ('Making a copy of filtered data...')
-#     x1 = raw.copy().pick(['MEG1113']).crop(tmin = 60., tmax = 120.) # filtered
-#     # plot these two copies using matplotlib
-#     from scipy import fftpack
-#     import matplotlib.pyplot as plt
-#     print('Plotting comparison between filtered and unfiltered data...')
-#     x0, times = x0[:,:]
-#     x0 = x0[0]
-#     x1, times = x1[:,:]
-#     x1 = x1[0]
-
-#     # you might need to use plt.ion() to turn on interactive mode
-#     fig_filter_effect, axes = plt.subplots(1,3, figsize = (12,6))
-#     axes[0].plot(times, x1)
-#     axes[0].plot(times, x0 - 1e-12)
-#     axes[0].set(xlabel = 'Time (sec)', xlim = [times[0], times[1000]])
-#     X0 = fftpack.fft(x0)
-#     X1 = fftpack.fft(x1)
-#     freqs = fftpack.fftfreq(len(x0), 1./raw.info['sfreq'])
-#     mask = freqs >= 0
-#     X0 = X0[mask]
-#     X1 = X1[mask]
-#     freqs = freqs[mask]
-#     axes[1].plot(freqs, 20 * np.log10(np.maximum(np.abs(X1), 1e-16)))
-#     axes[1].plot(freqs, 20 * np.log10(np.maximum(np.abs(X0), 1e-16)))
-#     axes[1].set(xlim = [0, 60], xlabel = 'Frequency (Hz)', ylabel = 'Magnitude (dB)')
-#     angles = (np.angle(X1) - np.angle(X0)) / (2 * np.pi)
-#     angles[np.absolute(angles) > 0.9] = angles[np.absolute(angles) > 0.9] + 1.
-#     axes[2].plot(freqs, angles)
-#     #axes[2].plot(freqs[1:], np.abs(np.remainder(np.angle(X1[1:]), 2. * np.pi) - np.remainder(np.angle(X0[1:]), 2 * np.pi)) / (2 * np.pi * freqs[1:]))
-#     #axes[2].plot(freqs, np.angle(X0))
-#     axes[2].set(xlim = [0, 60], xlabel = 'Frequency (Hz)', ylabel = 'Phase delay [cycle]')
-#     tight_layout()
-#     return raw_filtered, fig_filter_effect
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+    return fig
 
 
-def shift_event_photodiode(events, target_triggers=None, photodiode_trigger=512):
-    
+
+# =============================================================================
+# Epoched data preprocessing
+# =============================================================================
+
+def find_semantic_events(raw, subject='', semantic_triggers=''):
+    print('Finding semantic events.')
+    events = mne.find_events(raw, stim_channel='STI101', min_duration=0.002, 
+                         initial_event=True, verbose=False)
+    events_semantic = mne.pick_events(events, include=semantic_triggers)
+    if subject == 'sub-35':
+        # manually add an event because participant pressed on button box through these trials
+        events_to_add = [[507731, 0, 71], # low
+                         [511784, 0, 231]] # abstract baseline
+        events_added = np.concatenate((events_semantic, np.array(events_to_add)))
+        sort_indices = np.argsort(events_added[:,0])
+        events_semantic = events_added[sort_indices]
+    if len(events_semantic) < 900:
+        try:
+        #     print('Not finding 900 semantic events. Tring unit_cast method.')
+        #     events = mne.find_events(raw, stim_channel='STI101', min_duration=0.002, 
+        #                              uint_cast=True, verbose=False)
+        #     events_semantic = mne.pick_events(events, include=semantic_triggers)
+        #     assert(len(events_semantic) == 900)
+        # except AssertionError:
+        #     print('Not finding 900 semantic events. Tring consecutive=True.')
+        #     events = mne.find_events(raw, stim_channel='STI101', min_duration=0.002, 
+        #                              verbose=True, consecutive=False)
+        #     events_semantic = mne.pick_events(events, include=semantic_triggers)
+        #     assert(len(events_semantic) == 900)
+        # except AssertionError:
+            print('Not finding 900 semantic events. Trying a mask.')
+            events = mne.find_events(raw, stim_channel='STI101', min_duration=0.002, 
+                                     mask=8192, mask_type='not_and', verbose=False)
+            events_semantic = mne.pick_events(events, include=semantic_triggers)
+            assert(len(events_semantic) == 900)
+        except AssertionError:
+            print('Not finding 900 semantic events. Trying another mask.')
+            events = mne.find_events(raw, stim_channel='STI101', min_duration=0.002, 
+                                     mask=512, mask_type='not_and', verbose=False)
+            events_semantic = mne.pick_events(events, include=semantic_triggers)
+            assert(len(events_semantic) == 900)
+        finally:
+            print(f'Found {len(events_semantic)} semantic events.')
+    return events_semantic
+
+def correct_display_delay(events_semantic, events_photodiode):
     print('Checking photodiode-trigger delays.')
-    events_semantic = pick_events(events, include=target_triggers)
-    events_photodiode = pick_events(events, include=photodiode_trigger)
-    
     match = []
     for time_semantic in events_semantic[:,0]:
         for time_photodiode in events_photodiode[:,0]:
             if abs(time_photodiode - time_semantic) <= 50:
-                match.append([time_semantic, time_photodiode, time_photodiode-time_semantic])
+                match.append(time_photodiode-time_semantic)
                 
-    delay = np.abs(np.asarray(match)[:,2])
+    delay = np.abs(np.asarray(match))
     print('===')
     print(f'Number of matches (total target triggers={len(events_semantic)}): {len(delay)}')
     print(f'Mean latency: {delay.mean():.3f} ms')
     print(f'SD latency: {delay.std():.3f} ms')
     print('===')
-    return delay
+
+    if len(events_semantic) == len(delay):
+        print('Matching number of events :) Correcting delay using actual delay!')
+        events_semantic[:,0] += delay
+    else:
+        print('Mismatching number of events :( Correcting delay using mean delay.')
+        events_semantic[:,0] += round(delay.mean())
+        
+    return events_semantic
+
+def compare_reject_thresholds(autoreject, hardcoded):
+    ch_types = ['grad','mag','eeg']
+    composite = {ch_type: min(hardcoded[ch_type], autoreject[ch_type]) for ch_type in ch_types}
+    print("Composite reject dictionary:", composite)
+    return composite
+
+
+
+# =============================================================================
+# Decoding analysis helper functions
+# =============================================================================
+
+def extract_target_epochs(epochs, analysis=''):
+    if analysis == 'lexicality': 
+        epochs_word = epochs[(epochs.metadata.condition == 'mid') | 
+                             (epochs.metadata.condition == 'concrete_subsective') | 
+                             (epochs.metadata.condition == 'concrete_privative') | 
+                             (epochs.metadata.condition == 'abstract_subsective') | 
+                             (epochs.metadata.condition == 'abstract_privative')]
+        # 'nord' is short for non-word 
+        epochs_nord = epochs[(epochs.metadata.condition == 'low') | 
+                             (epochs.metadata.condition == 'high') | 
+                             (epochs.metadata.condition == 'concrete_baseline') | 
+                             (epochs.metadata.condition == 'abstract_baseline')]
+        epochs_word = mne.epochs.combine_event_ids(epochs_word, list(epochs_word.event_id.keys()), {'word': 99}, copy=True)
+        epochs_nord = mne.epochs.combine_event_ids(epochs_nord, list(epochs_nord.event_id.keys()), {'nord': 100}, copy=True)
+        epochs = mne.concatenate_epochs([epochs_word, epochs_nord])
+        epochs.crop(tmin=-0.2, tmax=0.6)
+    return epochs
+
+
+
+# =============================================================================
+# Calculation helpers
+# =============================================================================
+
+def calculate_analytical_chance(n_trial, alpha=0.05, class_ratio=0.5):
+    #n_trial = int(n_trial[0])
+    chance = binom.ppf(q=1-alpha,  # 1 minus alpha 
+                       n=n_trial,  # number of trials 
+                       p=max(class_ratio, 1-class_ratio)
+                       ) / n_trial # percent point function: inverse of cdf
+    return chance
 
 def calculate_avg_sem(array):
     avg = np.mean(array, axis=0)
