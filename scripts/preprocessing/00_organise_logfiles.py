967223ccaea39a39f86fbc1774591e50b358cc1f
Ryan Law <ryan.law@mrc-cbu.cam.ac.uk>
2026-01-27 10:31:53 +0000

    tidy preprocessing

--- scripts/preprocessing/00_organise_logfiles.py
+++ scripts/preprocessing/00_organise_logfiles.py
@@ -6,13 +6,18 @@ Created on Fri Sep  8 13:58:12 2023
 @author: rl05
 """
 
-import sys
+# import sys
 import os.path as op
+import numpy as np
 import pandas as pd
 
-
-sys.path.append('/imaging/hauk/rl05/fake_diamond/scripts/preprocessing')
+# sys.path.append('/imaging/hauk/rl05/fake_diamond/scripts/preprocessing')
 import config
+
+
+def count_letters(text):
+    return sum(c.isalpha() for c in text)    
+    
 
 subject_ids = config.subject_ids
 runs = [1,2,3,4,5]
@@ -64,18 +69,29 @@ for subject_id in subject_ids:
         subject_logs['condition'] = subject_logs.index.map(stimuli['condition_name'])
         subject_logs['experiment'] = subject_logs.index.map(stimuli['experiment'])
         subject_logs['experiment'] = subject_logs['experiment'].replace('concreteness_denotation','compose')
+        subject_logs['zipf_adj'] = subject_logs.index.map(stimuli['zipf_word1'])
+        subject_logs['zipf_noun'] = subject_logs.index.map(stimuli['zipf_word2'])
+        subject_logs['freq_dep'] = subject_logs.index.map(stimuli['freq_dep'])
+        subject_logs['freq_seq'] = subject_logs.index.map(stimuli['freq_seq'])
         subject_logs.reset_index(inplace=True)
-        # subject_logs[['concreteness', 'denotation']] = subject_logs.loc[:,'condition'].str.split('_', n=1, expand=True)
 
-        # # add two- vs. one-word info
-        # subject_logs['composition'] = ''
-        # subject_logs[subject_logs.]
 
+        # get word length
+        subject_logs['length_adj'] = subject_logs['word1'].apply(count_letters)
+        subject_logs['length_noun'] = subject_logs['word2'].apply(count_letters)
+        
+        # organise condition columns a bit better
+        composition_coding_dict = {'low': 'word', 'mid': 'phrase', 'high': 'word',
+                                    '^(.*)_baseline$': 'word', '^(.*)_subsective$': 'phrase', '^(.*)_privative$': 'phrase'}
+        subject_logs['composition'] = subject_logs['condition'].replace(composition_coding_dict, regex=True)
+        denotation_coding_dict = {'low': '', 'mid': '', 'high': '',
+                                    '^(.*)_baseline$': 'baseline', '^(.*)_subsective$': 'subsective', '^(.*)_privative$': 'privative'}
+        subject_logs['denotation'] = subject_logs['condition'].replace(denotation_coding_dict, regex=True)
+        concreteness_coding_dict = {'low': '', 'mid': '', 'high': '',
+                                    '^concrete_(.*)$': 'concrete', '^abstract_(.*)$': 'abstract'}
+        subject_logs['concreteness'] = subject_logs['condition'].replace(concreteness_coding_dict, regex=True)
+        subject_logs['specificity'] = np.where(subject_logs['condition'].isin(['low', 'mid', 'high']), subject_logs['condition'], '')
+        
         # save to CBU server
         subject_logs_fname = op.join(logs_dir_cbu, f'sub-{subject_id}_logfile.csv')
         subject_logs.to_csv(subject_logs_fname, index=False)
-
-        # # save to ownCloud
-        # subject_logs_fname = op.join(analysis_dir_ownCloud, f'sub-{subject_id}_logfile.csv')
-        # subject_logs.to_csv(subject_logs_fname, index=False)
-        
